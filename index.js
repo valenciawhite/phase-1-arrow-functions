5cfc1fc2bafb9db2f02eb55fba7a8ae206ff0489
Valencia White <valenciawhite@rocketmail.com>
2022-02-24 07:59:12 -0500

    commit tree e410d316d1247420879e2ca055e36cd5a64d4143

---
+++ index.js
@@ -0,0 +1,12 @@
+// function divide (num1, num2){
+//     return num1 / num2;
+// }
+
+let divide = function(num1 = 2000, num2 = 100){
+    return num1 / num2;
+};
+
+const square = num3 => num3 * num3;
+square(3,3);
+
+const add = (num4, num5) => num4 + num5;
